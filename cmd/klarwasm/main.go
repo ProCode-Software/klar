74a93ad2e3e5fe3c9d1145a052690fbff0db19cd
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-04-05 21:20:41 -0500

    Update wasm compiler

--- cmd/klarwasm/main.go
+++ cmd/klarwasm/main.go
@@ -1,41 +1,80 @@
+//go:build js && wasm
+
+// KlarWasm provides a WASM-compatible interface for the Klar compiler.
 package main
 
 import (
 	"encoding/json/v2"
 	"strings"
 	"syscall/js"
-	"time"
 
 	"github.com/ProCode-Software/klar/internal/build"
-	"github.com/ProCode-Software/klar/internal/errors"
+	"github.com/ProCode-Software/klar/internal/cli/ansi"
 	"github.com/ProCode-Software/klar/internal/errors/jsonerrors"
-	"github.com/ProCode-Software/klar/pkg/parser"
 )
 
 func main() {
-	js.Global().Set("compileKlar", js.FuncOf(func(this js.Value, args []js.Value) any {
-		return Parse(args[0].String())
-	}))
-	select {}
+	var parseFunc, freeFunc js.Func
+
+	parseFunc = js.FuncOf(func(this js.Value, args []js.Value) any {
+		s, report := Parse(args[0].String(), args[1].String())
+		var reportFunc js.Func
+		reportFunc = js.FuncOf(func(this js.Value, args []js.Value) any {
+			reportFunc.Release()
+			return report()
+		})
+		return js.ValueOf(map[string]any{
+			"output":       s,
+			"reportErrors": reportFunc, // Returns CLI-style diagnostics as a string
+		})
+	})
+	js.Global().Set("compileKlar", parseFunc)
+
+	// Function to free the compiler when the page is unloaded
+	freeFunc = js.FuncOf(func(js.Value, []js.Value) any {
+		parseFunc.Release()
+		freeFunc.Release()
+		return nil
+	})
+	js.Global().Set("freeCompiler", freeFunc)
+
+	select {} // Keep running
 }
 
-func Parse(s string) string {
-	prog, parseErrs, err := parser.ParseString(s)
+// Parse compiles the given source string and returns the result as a JSON string.
+func Parse(s string, fileName string) (out string, report func() string) {
+	var (
+		buf         strings.Builder
+		c, res, err = build.CompileString(s, fileName)
+		isMaxErrors = build.IsMaxErrors(err)
+	)
+	if err != nil || len(res.Errors) > 0 {
+		jsonerrors.WriteTo(&buf, res, err, isMaxErrors)
+	} else {
+		json.MarshalWrite(&buf, res)
+	}
+	return buf.String(), func() string {
+		return ReportErrors(c, res, err, isMaxErrors)
+	}
+}
+
+func ReportErrors(c *build.Compiler, res *build.Result, err error, isMaxErrors bool) string {
 	var buf strings.Builder
-	compileErrs := make([]errors.CompileError, len(parseErrs))
-	for i, e := range parseErrs {
-		compileErrs[i] = e
-	}
-	if err == nil && len(parseErrs) == 0 {
-		if err := json.MarshalWrite(&buf, prog); err != nil {
-			panic(err)
+	c.Reporter.UseColor = true
+	c.Reporter.Output = &buf
+
+	// Actual errors
+	c.PrintAllErrors(res.Errors)
+
+	// Critical error
+	if err != nil {
+		const prefix = "<**><r!>Error</r!><dim>:</dim> "
+		if ie, ok := err.(*build.InterfaceError); ok {
+			main, det := ie.PrettyError()
+			ansi.Fprintfln(&buf, prefix+"%s</**>%s", main, det)
+		} else {
+			ansi.Fprintfln(&buf, prefix+"%s</**>", err)
 		}
-		return buf.String()
 	}
-	jsonerrors.WriteTo(&buf, &build.BuildResult{
-		Errors:  compileErrs,
-		Modules: nil,
-		Elapsed: time.Duration(0),
-	}, err, false)
 	return buf.String()
 }
