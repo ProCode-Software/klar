0c4839c918dc7304cb17441697bc5d84dfa8802c
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-06-06 16:19:13 -0500

    Update version bumping script

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 set -e
-newVersion=$1
-packages=(**/package.json ./package.json)
+shopt -s nullglob globstar extglob
 
-for package in "${packages[@]}"; do
-    sed -i 's/"version": ".*"/"version": "'"${newVersion}"'"/' "$package"
+new_ver=$1
+
+if [ -z "$new_ver" ]; then
+    echo -e "\033[91mExpected a target version (Usage: $0 <version>)\033[m"
+    exit 1
+fi
+
+# Bump package.json versions
+mapfile -t package_jsons < <(
+    find . -path '*/node_modules' -prune -o -name package.json -print
+)
+for file in "${package_jsons[@]}"; do
+    sed -i 's/"version": ".*"/"version": "'"${new_ver}"'"/' "$file"
 done
 
-cat <<-EOF > ./internal/version/version.go
-package version
+# Bump glas.pack versions
+glas_pack_klar_vers=(./std/glas.pack)
+for file in "${glas_pack_klar_vers[@]}"; do
+    sed -E -i 's/version: .+$/version: v'"${new_ver}/" "$file"
+done
 
-const (
-    KlarVersion = "${newVersion}"
-    KlarPath = "$HOME/.klar"
-)
+# Bump minimum Klar version for sample projects
+glas_pack_klar_vers=(./samples/*/glas.pack ./std/glas.pack)
+for file in "${glas_pack_klar_vers[@]}"; do
+    sed -E -i 's/klar: .+$/klar: v'"${new_ver}/" "$file"
+done
 
-EOF
-
-echo "Bumped Klar version to v${newVersion}"
+echo -e "\e[92mBumped Klar version to v${new_ver}\e[m"
