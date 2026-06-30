ba7fbdd32aa3e72cc63b5850b824a33cd4dddbf0
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-06-29 19:43:42 -0500

    Parser: Move `public` parsing to general declaration parsing file
    `public` is now the only modifier, so it doesn't have to be in its own file.

--- internal/parser/declaration.go
+++ internal/parser/declaration.go
@@ -505,3 +505,39 @@ func (p *Parser) ParseAttribute() *ast.Attribute {
 	}
 	return d
 }
+
+func (p *Parser) validatePublic() {
+	if p.CurrKind() == lexer.Public {
+		p.Error(klarerrs.Token(klarerrs.ErrPublicGoesFirst, p.Curr()))
+	}
+}
+
+func (p *Parser) ParsePublicModifier() ast.Statement {
+	firstPublic := p.Expect(lexer.Public)
+	var stmt ast.Statement
+	switch curr := p.Curr(); curr.Kind {
+	case lexer.Public:
+		err := klarerrs.Token(klarerrs.ErrDuplicateModifier, curr)
+		err.SetParam("modifier", lexer.Public)
+		// Show where it was already used
+		err.Highlights = append(err.Highlights, klarerrs.Highlight{
+			ranges.FromToken(firstPublic), "It was already used here",
+		})
+		p.Error(err)
+		// Still parse it
+		stmt = &ast.BadExpression{Value: p.ParsePublicModifier()}
+		markStartEndPos(p, stmt, curr.Position)
+	default:
+		stmt = p.ParseStatement(noEOS)
+	}
+	switch stmt.(type) {
+	case *ast.BadExpression:
+	case *ast.FunctionDeclaration, ast.TypeDeclaration,
+		*ast.VariableDeclaration, *ast.FuncAliasDeclaration,
+		ast.ModifierDeclaration:
+		return &ast.PublicDeclaration{Declaration: stmt}
+	default:
+		p.Error(klarerrs.Node(klarerrs.ErrInvalidPublic, stmt))
+	}
+	return &ast.BadExpression{Value: stmt}
+}
