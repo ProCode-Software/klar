ab626b054dae614e66bc20054c55cb10e60febe9
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-06-30 20:35:56 -0500

    Give users the option to download prebuit Klar/Glas binaries

--- install.sh
+++ install.sh
@@ -9,11 +9,13 @@ for arg in "$@"; do
     case "$arg" in
     --global) global=1 ;;
     --add-to-path) add_to_path=1 ;;
+    --prebuild) use_prebuild=1 ;;
     --help | -h)
         echo "Flags:
     --global       Install Klar globally (requires sudo)
     --add-to-path  Add Klar to PATH
     --help         Show this help message
+    --prebuild     Install a prebuild instead of building from source
 
 https://github.com/ProCode-Software/klar"
         exit 0
@@ -26,15 +28,6 @@ done
 
 if [[ -n $add_to_path && $global -eq 1 ]]; then
     red "Can't enable '--add-to-path' with '--global'" && exit 1
-fi
-
-# Ensure we have Git and Go installed
-if ! command -v git &> /dev/null; then
-    red "git is required to install Klar. Install it at https://git-scm.com." && exit 1
-fi
-
-if ! command -v go &> /dev/null; then
-    red "Go is required to install Klar. Install it at https://go.dev." && exit 1
 fi
 
 if [[ -z $global ]]; then
@@ -53,53 +46,145 @@ if [[ -z $global ]]; then
         esac
         break
     done
-    echo
 fi
+if [[ -z $use_prebuild ]]; then
+    yellow "Do you want to build from source, or use a prebuilt binary?"
+    echo -e "Building from source makes the latest features and fixes available, but requires Git and the Go toolchain to be installed, and may take longer to install. Downloading a prebuilt binary is faster, but may not include the latest Klar fixes."
+    select build_type in "Build from source" "Download a prebuilt binary"; do
+        case "$build_type" in
+        "Build from source") use_prebuild=0 ;;
+        "Download a prebuilt binary") use_prebuild=1 ;;
+        esac
+        break
+    done
+fi
+
+echo
 
 build_dir=$(mktemp -d)
 cd "$build_dir" || exit
 
-# Clone Klar repository
-progress "📖 Cloning Klar repository..."
-set +e
-err=$(git clone https://github.com/ProCode-Software/klar.git . 2>&1)
-status=$?
-set -e
-if [[ $status -ne 0 ]]; then
-    red "Failed to clone Klar repository. The error was:"
-    echo "$err"
-    exit 1
+get_exec() {
+    GOOS=$1
+    klar_exec="klar"
+    glas_exec="glas"
+    if [[ $GOOS == "windows" ]]; then
+        klar_exec+=".exe"
+        glas_exec+=".exe"
+    fi
+}
+
+download_prebuild() {
+    # Keys: Output from uname; Values: Binary OS/arch names
+    declare -A oses=(
+        [darwin]=macos
+        [macos]=macos
+        [linux]=linux
+        [windows]=windows
+    ) arches=(
+        [x86_64]=x86_64
+        [x64]=x86_64
+        [arm64]=arm64
+        [aarch64]=arm64
+    )
+    os=$(uname -s)
+    prebuild_os_name=${oses[${os,,}]}
+    if [[ -z $prebuild_os_name ]]; then
+        red "Unfortunately, we don't provide prebuilds for the $os operating system :(
+Please build from source instead by rerunning without the '--prebuild' flag" && exit 1
+    fi
+    arch=$(uname -m)
+    prebuild_arch_name=${arches[${arch,,}]}
+    if [[ -z $prebuild_arch_name ]]; then
+        red "Unfortunately, we don't provide prebuilds for the $arch architecture :(
+Please build from source instead by rerunning without the '--prebuild' flag" && exit 1
+    fi
+
+    # Find the name of the GitHub release asset
+    progress "📦 Downloading prebuilt Klar and Glas binaries..."
+    products=(klar glas)
+    if ! command -v jq &> /dev/null; then
+        # Alternative for Windows (Git Bash) users without jq
+        binary_name=$(curl -s "https://api.github.com/repos/ProCode-Software/klar/releases/latest" |
+            grep -oE '"browser_download_url":[ ]*"[^"]+"' |
+            sed -E 's/"browser_download_url":[ ]*"([^"]+)"/\1/' |
+            grep -E 'klar-.*'"$prebuild_os_name"'-'"$prebuild_arch_name"'.*' |
+            head -n 1)
+    else
+        binary_name=$(curl -s "https://api.github.com/repos/ProCode-Software/klar/releases/latest" |
+            jq -r '.assets[] | select(.name | test("^klar-.*'"$prebuild_os_name"'-'"$prebuild_arch_name"'")) | .browser_download_url')
+    fi
+
+    if [[ -z $binary_name ]]; then
+        red "Unfortunately, we couldn't find a prebuilt binary for $prebuild_os_name-$prebuild_arch_name.
+Please build from source instead by rerunning without the '--prebuild' flag" && exit 1
+    fi
+
+    # Download Klar and Glas
+    get_exec "$prebuild_os_name"
+    for product in "${products[@]}"; do
+        product_url=${binary_name//klar/$product}
+        product_exec=${product}_exec
+        curl -fsSL -o "$build_dir/${!product_exec}" "$product_url"
+    done
+
+    # Download the standard library
+    progress "📚 Downloading the standard library..."
+    curl -fsSL -o "$build_dir/stdlib.zip" "https://github.com/ProCode-Software/klar/releases/latest/download/stdlib.zip"
+    unzip -o "$build_dir/stdlib.zip" -d "$build_dir"
+}
+
+build_from_source() {
+    # Ensure we have Git and Go installed
+    if ! command -v git &> /dev/null; then
+        red "git is required to install Klar. Install it at https://git-scm.com." && exit 1
+    fi
+
+    if ! command -v go &> /dev/null; then
+        red "Go is required to install Klar. Install it at https://go.dev." && exit 1
+    fi
+
+    # Clone Klar repository
+    progress "📖 Cloning Klar repository..."
+    set +e
+    err=$(git clone https://github.com/ProCode-Software/klar.git . 2>&1)
+    status=$?
+    set -e
+    if [[ $status -ne 0 ]]; then
+        red "Failed to clone Klar repository. The error was:"
+        echo "$err"
+        exit 1
+    fi
+
+    # Get the current GOOS and GOARCH
+    GOOS=$(uname -s)
+    export GOOS=${GOOS,,}
+
+    case "$(uname -m)" in
+    x86_64) GOARCH=amd64 ;;
+    arm64) GOARCH=arm64 ;;
+    i386 | i686) GOARCH=386 ;;
+    *) red "Unsupported architecure: $(uname -m)" && exit 1 ;;
+    esac
+    export GOARCH
+
+    # Build Klar and Glas executables
+    export GOEXPERIMENT=jsonv2
+    VERSION="0.1.0"
+    LDFLAGS="-X 'github.com/ProCode-Software/klar/internal/cli.KlarVersion=$VERSION'"
+
+    get_exec "$GOOS"
+    progress "🏗️ Building Klar and Glas binaries..."
+    go generate ./...
+    go build -ldflags="$LDFLAGS" -o "$klar_exec" ./cmd/klar
+    go build -ldflags="$LDFLAGS" -o "$glas_exec" ./cmd/glas
+}
+
+if [[ $use_prebuild -eq 1 ]]; then
+    download_prebuild
+else
+    build_from_source
 fi
-
-# Get the current GOOS and GOARCH
-GOOS=$(uname -s)
-export GOOS=${GOOS,,}
-
-case "$(uname -m)" in
-x86_64) GOARCH=amd64 ;;
-arm64) GOARCH=arm64 ;;
-i386 | i686) GOARCH=386 ;;
-*) red "Unsupported architecure: $(uname -m)" && exit 1 ;;
-esac
-export GOARCH
-
-# Build Klar and Glas executables
-export GOEXPERIMENT=jsonv2
-VERSION="0.1.0"
-LDFLAGS="-X 'github.com/ProCode-Software/klar/internal/cli.KlarVersion=$VERSION'"
-
-klar_exec="klar"
-glas_exec="glas"
-
-if [ "$GOOS" = "windows" ]; then
-    klar_exec+=".exe"
-    glas_exec+=".exe"
-fi
-
-progress "🏗️ Building Klar and Glas binaries..."
-go generate ./...
-go build -ldflags="$LDFLAGS" -o "$klar_exec" ./cmd/klar
-go build -ldflags="$LDFLAGS" -o "$glas_exec" ./cmd/glas
 
 # Install Klar and Glas to bin directory
 progress "🚚 Installing Klar and Glas..."
@@ -131,7 +216,7 @@ case "$GOOS $global" in
 esac
 
 mkdir -p "$STD_DIR"
-cp -R ./std "$STD_DIR"
+cp -R ./std/* "$STD_DIR"
 
 echo -e "
 \e[1;92m🐨 Klar has been successfully installed!\e[m
