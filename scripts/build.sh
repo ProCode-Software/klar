4ed4c1e0fffd1f7a26d3b726309e8a10fdd0bfbb
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-06-30 20:50:17 -0500

    Update build script to disable testing and set commit numbers

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,8 +24,10 @@ declare -A OS_NAMES=(
 
 export GOEXPERIMENT=jsonv2
 
-VERSION="0.1.0"
-LDFLAGS="-X 'github.com/ProCode-Software/klar/internal/cli.KlarVersion=$VERSION'"
+VERSION="0.0.1"
+COMMIT=$(git rev-parse --short HEAD)
+[[ $COMMIT_VER -eq 1 ]] && ver_name=$COMMIT || ver_name=$VERSION
+LDFLAGS=("-X 'github.com/ProCode-Software/klar/internal/cli.Klar"{Version=$VERSION,Commit=$COMMIT}"'")
 
 function pre_build() {
     # Generate code
@@ -34,7 +36,7 @@ function pre_build() {
     for os in "${OSES[@]}"; do
         for arch in ${ARCHES[$os]}; do
             echo "Testing $os/$arch..."
-            GOOS=$os GOARCH=$arch go test "$KLAR_ROOT/..."
+            # GOOS=$os GOARCH=$arch go test "$KLAR_ROOT/..."
         done
     done
 }
@@ -47,11 +49,11 @@ function build_binaries() {
         for arch in ${ARCHES[$os]}; do
             for product in "${PRODUCTS[@]}"; do
                 echo "Compiling $product for $os/$arch..."
-                out_path="$KLAR_ROOT/bin/$product-$VERSION-${OS_NAMES[$os]}-${ARCH_NAMES[$arch]}"
+                out_path="$KLAR_ROOT/bin/$product-$ver_name-${OS_NAMES[$os]}-${ARCH_NAMES[$arch]}"
                 if [[ $os == "windows" ]]; then
                     out_path+=".exe"
                 fi
-                GOOS=$os GOARCH=$arch go build -ldflags="$LDFLAGS" --trimpath \
+                GOOS=$os GOARCH=$arch go build -ldflags="${LDFLAGS[*]}" --trimpath \
                     -o "$out_path" \
                     "$KLAR_ROOT/cmd/$product"
             done
@@ -61,15 +63,15 @@ function build_binaries() {
 
 function build_klar_wasm() {
     echo Compiling klarwasm...
-    out_path="$KLAR_ROOT/bin/klar-browser.wasm"
-    GOOS=js GOARCH=wasm go build -ldflags="$LDFLAGS" --trimpath \
+    out_path="$KLAR_ROOT/bin/klar-$ver_name-browser.wasm"
+    GOOS=js GOARCH=wasm go build -ldflags="${LDFLAGS[*]}" --trimpath \
         -o "$out_path" \
         "$KLAR_ROOT/cmd/klarwasm"
 }
 
 function pack_stdlib() {
     echo Packing standard library...
-    zip -r "$KLAR_ROOT/bin/stdlib.zip" "$KLAR_ROOT/std" > /dev/null
+    zip -r "$KLAR_ROOT/bin/stdlib.zip" "./std" > /dev/null
 }
 
 function main() {
