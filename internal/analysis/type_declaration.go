c1507ec09c0fb8045fefe0a9398ff7d1fa744108
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-07-08 20:12:51 +0000

    Analysis: Reorganize type checker

--- internal/analysis/type_declaration.go
+++ internal/analysis/type_declaration.go
@@ -31,6 +31,71 @@ func (n *TypeName) String() string {
 func (n *TypeName) Underlying() Type { return n.Type }
 func (n *TypeName) objKind()         {}
 
+// Alias
+// =======
+
+type TypeAlias struct {
+	Name string
+	Type
+	resolved Type
+}
+
+func (a *TypeAlias) Resolve() Type {
+	if a.resolved == nil {
+		if subAlias, ok := a.Type.(*TypeAlias); ok {
+			// This also recursively resolves aliases for upstream type aliases
+			a.resolved = subAlias.Resolve()
+		} else {
+			// a's underlying type isn't an alias, so it is already resolved
+			a.resolved = a.Type
+		}
+	}
+	return a.resolved
+}
+
+func (a *TypeAlias) Kind() Kind       { return a.Resolve().Kind() }
+func (a *TypeAlias) Underlying() Type { return a.Resolve() }
+
+func (c *Checker) checkTypeAlias(o *Object, node *ast.TypeAliasDeclaration) {
+	fctx := o.LookupContext()
+	tn := o.typ.(*TypeName)
+	alias := &TypeAlias{Name: o.name}
+	tn.Type = alias
+	rhs := c.parseType(node.Type, fctx)
+	// Set to invalid if we couldn't typecheck the rhs
+	if rhs == nil {
+		rhs = InvalidType
+	}
+	if rhs.Kind() == KindGeneric {
+		// The target of a type alias cannot be a generic type
+		err := klarerrs.Range(klarerrs.ErrGenericTypeAlias, node.Type.GetRange())
+		err.Label = "This can't be a generic type"
+		c.fileError(err, o.file)
+		rhs = InvalidType
+	}
+	alias.Type = rhs
+}
+
+func (c *Checker) checkFuncAlias(o *Object) {
+	decl := o.info
+	targetExpr := decl.node.(*ast.FuncAliasDeclaration).Target
+	// TODO: Lookup the target expression and make sure it resolves to a function
+	var target *Object = nil
+
+	_ = targetExpr
+	o.typ.(*FunctionAlias).Target = target
+}
+
+func Unalias(t Type) Type {
+	if a, ok := t.(*TypeAlias); ok {
+		return a.Resolve()
+	}
+	return t
+}
+
+// Tag
+// ======
+
 // Tag represents a Klar tag type.
 type Tag struct{ Implements map[Type]struct{} }
 
