68eff02c13cf1c1db728ef2303f4d12f87d41b1d
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-08-04 12:45:52 -0500

    State that `klar clean` deletes REPL history and crash dumps

--- cmd/klar/internal/clean/clean.go
+++ cmd/klar/internal/clean/clean.go
@@ -78,7 +78,9 @@ func ClearCache(clearGlobalCache, clearProjCache bool) (globalCacheSize, projCac
 
 const LongDescription = `When building a project, Klar caches each built module to avoid recompiling everything, including unmodified modules, every time you build, making following builds faster. However, this takes up disk space, and deleted modules may still remain in cache.
 
-You can occasionally clean the cache by running 'klar clean' to delete all cached modules. This doesn't delete or modify any of your Klar files. Remember that caching modules avoids typechecking them when they aren't modified, so deleting the cache will require recompiling the module. After running 'klar clean', the next build will take longer, but after building once, the cache will be resaved and build times will be faster for the next build onwards.`
+You can occasionally clean the cache by running 'klar clean' to delete all cached modules. This doesn't delete or modify any of your Klar files. Remember that caching modules avoids typechecking them when they aren't modified, so deleting the cache will require recompiling the module. After running 'klar clean', the next build will take longer, but after building once, the cache will be resaved and build times will be faster for the next build onwards.
+
+'klar clean' also deletes REPL history and crash dumps.`
 
 func deleteDir(dir string) (size int64, err error) {
 	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
